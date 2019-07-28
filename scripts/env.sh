dd73c6f05f1c74d639e31b6ffba3aa68212ecbcf
Gary Davies <gary.davies01@bbc.co.uk>
2019-07-28 17:33:16 +0100

    Echo out export

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export APPSYNC_API_KEY=$(aws ssm get-parameters --region eu-west-1 --names "APPSYNC_API_KEY" --query Parameters[0].Value)
-export APPSYNC_URL=$(aws ssm get-parameters --region eu-west-1 --names "APPSYNC_URL" --query Parameters[0].Value)
-export DISCORD_API_KEY=$(aws ssm get-parameters --region eu-west-1 --names "DISCORD_API_KEY" --query Parameters[0].Value)
+echo 'export APPSYNC_API_KEY='$(aws ssm get-parameters --region eu-west-1 --names "APPSYNC_API_KEY" --query Parameters[0].Value)
+echo 'export APPSYNC_URL='$(aws ssm get-parameters --region eu-west-1 --names "APPSYNC_URL" --query Parameters[0].Value)
+echo 'export DISCORD_API_KEY='$(aws ssm get-parameters --region eu-west-1 --names "DISCORD_API_KEY" --query Parameters[0].Value)
