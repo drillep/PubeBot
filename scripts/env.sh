bc36f6b89ad65d701aa85f19b8e2cd0360a56362
Gary Davies <gary.davies01@bbc.co.uk>
2019-07-28 14:58:55 +0100

    Add env script

---
+++ scripts/env.sh
@@ -0,0 +1,3 @@
+APPSYNC_API_KEY=$(aws ssm get-parameters --region eu-west-1 --names APPSYNC_API_KEY --query Parameters[0].Value)
+APPSYNC_URL=$(aws ssm get-parameters --region eu-west-1 --names APPSYNC_URL --query Parameters[0].Value)
+DISCORD_API_KEY=$(aws ssm get-parameters --region eu-west-1 --names DISCORD_API_KEY --query Parameters[0].Value)
