ca69d10e7d64683df9264a2e4c37ba8a003c6d29
Gary Davies <gary.davies01@bbc.co.uk>
2019-07-28 15:17:41 +0100

    Try export the parameters

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-APPSYNC_API_KEY=$(aws ssm get-parameters --region eu-west-1 --names APPSYNC_API_KEY --query Parameters[0].Value)
-APPSYNC_URL=$(aws ssm get-parameters --region eu-west-1 --names APPSYNC_URL --query Parameters[0].Value)
-DISCORD_API_KEY=$(aws ssm get-parameters --region eu-west-1 --names DISCORD_API_KEY --query Parameters[0].Value)
+export APPSYNC_API_KEY=$(aws ssm get-parameters --region eu-west-1 --names APPSYNC_API_KEY --query Parameters[0].Value)
+export APPSYNC_URL=$(aws ssm get-parameters --region eu-west-1 --names APPSYNC_URL --query Parameters[0].Value)
+export DISCORD_API_KEY=$(aws ssm get-parameters --region eu-west-1 --names DISCORD_API_KEY --query Parameters[0].Value)
