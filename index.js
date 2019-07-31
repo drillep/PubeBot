bf3b9db8453c79a6210b270d964e8daeae1158fa
Gary Davies <gary.davies01@bbc.co.uk>
2019-07-31 01:57:02 +0100

    Fetch parameters at client login

--- index.js
+++ index.js
@@ -103,52 +103,50 @@ const helpCommand = (arguments, recievedMessage) => {
 }
 
 const getMatchStats = async (arguments, recievedMessage, primaryCommand) => {
-  ssmConfig()
-  .then(params => {
-    fetch(params.appSyncApiKey, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'x-api-key': params.appSyncUrl
-        },
-        body: JSON.stringify({
-          query: `{ matchData(playerName: \"${primaryCommand}\") { startedAt DBNOs assists boosts damageDealt deathType headshotKills heals killPlace killStreaks kills longestKill name playerId revives rideDistance roadKills swimDistance teamKills timeSurvived vehicleDestroys walkDistance weaponsAcquired winPlace } }`
-        }),
-      })
-      .then(res => res.json())
-      .then(res => {
-        let startedAt = res.data.matchData.startedAt
-        let startTime = startedAt.slice(-9, -4)
-        let startDate = startedAt.substr(0, 10)
-        let dArr = startDate.split("-"); // ex input "2010-01-18"
-        startDate = dArr[2] + "/" + dArr[1] + "/" + dArr[0].substring(2);
-        recievedMessage.channel.send({
-          embed: {
-            color: 3447003,
-            // title: `${res.data.matchData.name} date and time`,
-            fields: [{
-              name: `${res.data.matchData.name} stats for ${startTime} ${startDate}`,
-              value: "Kills: " + res.data.matchData.kills + "\n" +
-                "Assists: " + res.data.matchData.assists + "\n" +
-                "Headshot Kills: " + res.data.matchData.headshotKills + "\n" +
-                "Kill Streaks: " + res.data.matchData.killStreaks + "\n" +
-                "Longest Kill: " + res.data.matchData.longestKill.toFixed(2) + "\n" +
-                "Team Kills: " + res.data.matchData.teamKills + "\n" +
-                "Revives: " + res.data.matchData.revives + "\n" +
-                "Time Survived: " + res.data.matchData.timeSurvived.toFixed(2) + "\n" +
-                "Damage Dealt: " + res.data.matchData.damageDealt.toFixed(2) + "\n" +
-                "Death Type: " + res.data.matchData.deathType + "\n" +
-                "Kill Place: " + res.data.matchData.killPlace + "\n" +
-                "Win Place: " + res.data.matchData.winPlace,
-              inline: true
-            }]
-          }
-        })
+  fetch(params.appSyncUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': params.appSyncApiKey
+      },
+      body: JSON.stringify({
+        query: `{ matchData(playerName: \"${primaryCommand}\") { startedAt DBNOs assists boosts damageDealt deathType headshotKills heals killPlace killStreaks kills longestKill name playerId revives rideDistance roadKills swimDistance teamKills timeSurvived vehicleDestroys walkDistance weaponsAcquired winPlace } }`
+      }),
+    })
+    .then(res => res.json())
+    .then(res => {
+      let startedAt = res.data.matchData.startedAt
+      let startTime = startedAt.slice(-9, -4)
+      let startDate = startedAt.substr(0, 10)
+      let dArr = startDate.split("-"); // ex input "2010-01-18"
+      startDate = dArr[2] + "/" + dArr[1] + "/" + dArr[0].substring(2);
+      recievedMessage.channel.send({
+        embed: {
+          color: 3447003,
+          // title: `${res.data.matchData.name} date and time`,
+          fields: [{
+            name: `${res.data.matchData.name} stats for ${startTime} ${startDate}`,
+            value: "Kills: " + res.data.matchData.kills + "\n" +
+              "Assists: " + res.data.matchData.assists + "\n" +
+              "Headshot Kills: " + res.data.matchData.headshotKills + "\n" +
+              "Kill Streaks: " + res.data.matchData.killStreaks + "\n" +
+              "Longest Kill: " + res.data.matchData.longestKill.toFixed(2) + "\n" +
+              "Team Kills: " + res.data.matchData.teamKills + "\n" +
+              "Revives: " + res.data.matchData.revives + "\n" +
+              "Time Survived: " + res.data.matchData.timeSurvived.toFixed(2) + "\n" +
+              "Damage Dealt: " + res.data.matchData.damageDealt.toFixed(2) + "\n" +
+              "Death Type: " + res.data.matchData.deathType + "\n" +
+              "Kill Place: " + res.data.matchData.killPlace + "\n" +
+              "Win Place: " + res.data.matchData.winPlace,
+            inline: true
+          }]
+        }
       })
     })
 }
+let params = {}
 ssmConfig()
-.then(params => {
-  client.login(params.discoredApiKey);
-})
-
+  .then(parameterStore => {
+    client.login(params.discoredApiKey)
+    params = parameterStore
+  })
